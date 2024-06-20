7327a6847d090d3fb8decbdd361832fd16dfba7e
Jihye Kim <wlgp115@gmail.com>
2024-06-20 11:13:16 +0900

    Initial commit

---
+++ src/main/java/practice/shoppingmallFinal/ShoppingmallFinalApplication.java
@@ -0,0 +1,13 @@
+package practice.shoppingmallFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/shoppingmallFinal/ShoppingmallFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.shoppingmallFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
